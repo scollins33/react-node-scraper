c05ff07e9e6f4236686ec4ba2bd685185df2c1dc
scollins33 <sean_collins@gmx.com>
2020-03-17 02:06:34 -0400

    starting acct mgr component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
-//
+import { AccountManager } from "./components/AccountManager";
+
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <AccountManager compiler="TypeScript" framework="React" />,
     document.getElementById("index")
 );
